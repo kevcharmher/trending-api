83a23e0341dd6ed7a5b44302724eb5a24e56e093
Kevin Her <kevcharmher@Kevins-iMac.local>
2018-12-31 09:59:01 -0800

    Add first route handler

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('WELCOME');
+});
+
 app.listen(port, () => {
   console.log('Listening on port ' + port);
 });
