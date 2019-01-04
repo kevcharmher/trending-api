959165a9976dcb167e378178b97c0c0e062aaf9d
Kevin Her <kevcharmher@Kevins-iMac.local>
2019-01-03 18:10:04 -0800

    Save updated server.js

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-var express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+
+const express = require('express');
 var app = express();
 
-const port = process.env.PORT || 3000;
+var array = [];
+var array2 = ['Kevin', 'Bear', 'CP3'];
+// request('http://tweeplers.com/hashtags/?cc=US', (error, response, html) => {
+//   if (!error && response.statusCode == 200) {
+//     const $ = cheerio.load(html);
+//
+//     const x = $('.col-xs-8.wordwrap').each((i, el) => {
+//       var z = $(el).text();
+//       console.log(z);
+//     });
+//   }
+// });
 
 app.get('/', (req, res) => {
-  res.send('WELCOME');
+  res.json(array2);
 });
 
-app.listen(port, () => {
-  console.log('Listening on port ' + port);
+app.get('/api/trending', (req, res) => {
+  array = [];
+  request('https://trends24.in/united-states/', (error, response, html) => {
+    if (!error & (response.statusCode == 200)) {
+      const $ = cheerio.load(html);
+
+      const xy = $('.trend-card__list li')
+        .slice(0, 10)
+        .each((i, el) => {
+          let text = $(el).text();
+          array.push(text);
+        });
+      console.log(array);
+      res.json(array);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Now running on port 3000');
 });
